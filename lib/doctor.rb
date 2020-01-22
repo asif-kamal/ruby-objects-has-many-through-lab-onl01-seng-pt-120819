dda47a01c526cd85f0590bce0ab982bbd055b652
Asif Kamal <asif_kamal@hotmail.com>
2020-01-22 22:17:08 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.collect{|appt| appt.doctor = self}
+    Appointment.all.select{|appt| appt.doctor == self}
   end
   
   def patients
