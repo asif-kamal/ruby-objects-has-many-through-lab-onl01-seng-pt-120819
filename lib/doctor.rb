91438ff2fcdb33e53e8f1df7f4bbabfb53bc137b
Asif Kamal <asif_kamal@hotmail.com>
2020-01-22 22:11:22 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.collect{|appt| appt.doctor = self}
+    Appointment.all.collect{|appt| appt.doctor == self}
   end
   
   def patients
